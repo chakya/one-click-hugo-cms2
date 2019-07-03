9b5297804c5ed4118334b6aa02d4f5cd6d068191
chakya <charles@rushowl.sg>
2019-07-03 16:06:24 +0700

    update transprot

--- site/themes/restaurant-hugo/static/js/main.js
+++ site/themes/restaurant-hugo/static/js/main.js
@@ -31,6 +31,16 @@ $(document).ready(function(){
     slideSpeed : 800,
  });
 
+  $("#owl-transportation").owlCarousel({
+    // Most important owl features
+    items : 4,
+    pagination : true,
+    paginationSpeed : 1000,
+    navigation : true,
+    navigationText : ["","<i class='fa fa-angle-right'></i>"],
+    slideSpeed : 800,
+ });
+
  $("#owl-entertainment").owlCarousel({
     // Most important owl features
     items : 4,
